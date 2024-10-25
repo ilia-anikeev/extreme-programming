554c74339ad3b7d4903c81cb08c05a07ab63f43d
ilia <ilia.anikeev@gmail.com>
2024-10-25 14:52:05 +0300

    add invalid password test

--- server/src/test/java/com/listManager/UnitTests.java
+++ server/src/test/java/com/listManager/UnitTests.java
@@ -1,18 +1,52 @@
 package com.listManager;
 
+import com.listManager.dto.UserLoginDto;
 import com.listManager.dto.UserRegistrationDto;
+import com.listManager.exceptions.InvalidPassword;
 import com.listManager.exceptions.UserAlreadyExist;
-import com.listManager.exceptions.InvalidEmail;
+import com.listManager.exceptions.UserNotFoundException;
 import com.listManager.model.*;
 import com.listManager.repository.*;
 import com.listManager.service.UserService;
-import jdk.jfr.DataAmount;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UnitTests {
     private final UserManager userManager = new UserManager();
     private final UserService userService = new UserService(userManager);
+
+    @Test
+    void successfulAuthorization() {
+        String username = "bebrinskiy";
+        String password = "password";
+        UserRegistrationDto userRegistrationDto = new UserRegistrationDto(username, password);
+        UserLoginDto userLoginDto = new UserLoginDto(username, password);
+        try {
+            userService.registerUser(userRegistrationDto);
+
+            User user = userService.loginUser(userLoginDto);
+            userManager.deleteUser(user.getUserID());
+        } catch (UserAlreadyExist | InvalidPassword | UserNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void invalidPassword() {
+        String username = "bebrinskiy";
+        String password = "password";
+        UserRegistrationDto userRegistrationDto = new UserRegistrationDto(username, password);
+        try {
+            User user = userService.registerUser(userRegistrationDto);
+
+            String invalidPassword = "invalid";
+            UserLoginDto userLoginDto = new UserLoginDto(username, invalidPassword);
+            assertThrows(InvalidPassword.class, () -> userService.loginUser(userLoginDto));
+            userManager.deleteUser(user.getUserID());
+        } catch (UserAlreadyExist e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     void createUserTest() {
@@ -22,7 +56,7 @@ class UnitTests {
             User user = userManager.findByUsername("bebrinskiy");
             assertEquals("bebrinskiy", user.getUsername());
             userManager.deleteUser(user.getUserID());
-        } catch (UserAlreadyExist | InvalidEmail e) {
+        } catch (UserAlreadyExist e) {
             throw new RuntimeException(e);
         }
     }
@@ -34,7 +68,7 @@ class UnitTests {
             User user = userService.registerUser(userRegistrationDto);
             assertThrows(UserAlreadyExist.class, () -> userService.registerUser(userRegistrationDto));
             userManager.deleteUser(user.getUserID());
-        } catch (UserAlreadyExist | InvalidEmail e) {
+        } catch (UserAlreadyExist e) {
             throw new RuntimeException(e);
         }
     }
